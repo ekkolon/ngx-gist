27f988c6dd57c5b3517da100b28bd1694feeceac
Nelson Dominguez <18242749+ekkolon@users.noreply.github.com>
2023-02-02 19:29:27 +0100

    feat(playground): :sparkles: scaffold playground app

---
+++ projects/ngx-gist-playground/src/app/app.component.ts
@@ -0,0 +1,18 @@
+/**
+ * @license
+ * Copyright Nelson Dominguez All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at the root of this project.
+ */
+
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'ngx-gist-playground-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'ngx-gist-playground';
+}
